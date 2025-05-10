04e4aba146ee6ed9c5028ecec9a1f01fae4e4168
tamarbrann <brann@post.bgu.ac.il>
2025-05-10 17:01:20 +0300

    js

---
+++ PART_B/js/products.js
@@ -0,0 +1,46 @@
+function showProducts(products) {
+  const container = document.getElementById("products");
+  for (const product of products) {
+    const div = document.createElement("div");
+
+    const img = document.createElement("img");
+    img.src = product.img;
+    img.alt = product.title;
+    div.appendChild(img);
+
+    const h3 = document.createElement("h3");
+    h3.innerText = product.title;
+    div.appendChild(h3);
+
+    const desc = document.createElement("p");
+    desc.innerText = product.desc;
+    div.appendChild(desc);
+
+    const price = document.createElement("div");
+    price.innerText = "Price: $" + product.price;
+
+    const button = document.createElement("button");
+    button.innerText = "Add to cart";
+
+    container.appendChild(div);
+  }
+}
+
+const allProducts = [
+  {
+    title: "Blue Baby Overalls",
+    desc: "Organic cotton, second hand, excellent condition.",
+    price: 10,
+    img: "picture/overalls.png",
+    color: "blue",
+  },
+  {
+    title: "Baby Dress",
+    desc: "Eco-friendly materials, first hand.",
+    price: 15,
+    img: "picture/dress.png",
+    color: "green",
+  },
+];
+
+showProducts(allProducts);
