58e6fc3d77305618142c6dee73ffe1954ef05e9b
tamarbrann <brann@post.bgu.ac.il>
2025-06-18 20:45:16 +0300

    cart page

---
+++ PART_B/js/cart.js
@@ -0,0 +1,43 @@
+const CART = 'cart';
+
+function showProducts(products) {
+  const container = document.getElementById("products");
+  container.innerHTML = "";
+
+  for (const product of products) {
+    const div = document.createElement("div");
+
+    const img = document.createElement("img");
+    img.src = product.img;
+    img.alt = product.title;
+    div.appendChild(img);
+
+    const h3 = document.createElement("h3");
+    h3.innerText = product.title;
+    div.appendChild(h3);
+
+    const desc = document.createElement("p");
+    desc.innerText = product.desc;
+    div.appendChild(desc);
+
+    const price = document.createElement("div");
+    price.innerText = "Price: $" + product.price;
+    div.appendChild(price);
+
+    const button = document.createElement("button");
+    button.innerText = "Remove from cart";
+    button.onclick = function() {
+      const cart = JSON.parse(localStorage.getItem(CART)) ?? [];
+      const filtered = cart.filter((p) => p.title !== product.title);
+      localStorage.setItem(CART, JSON.stringify(filtered));
+      showProducts(filtered);
+    }
+    div.appendChild(button);
+
+    container.appendChild(div);
+  }
+}
+
+const productsInCart =  JSON.parse(localStorage.getItem(CART)) ?? [];
+showProducts(productsInCart);
+
